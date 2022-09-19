5507994c73d6ea4bf532f0f0e3ff6cfd69b006ab
msx98 <me@glub.xyz>
2022-09-19 08:58:11 +0300

    Corrected dir paths

--- volumes/userscripts/pre-build.sh
+++ volumes/userscripts/pre-build.sh
@@ -1,28 +1,30 @@
-REPODIR=$SRC_DIR/LINEAGE_19_1
-cd $REPODIR
-bash $REPODIR/patches/picks.sh
+REPO_DIR=$SRC_DIR/LINEAGE_19_1
+PATCHES_DIR=/srv/patches
+
+cd $REPO_DIR
+bash $PATCHES_DIR/picks.sh
 
 echo Applying hardware/libhardware
-cd $REPODIR
-cp patches/patches/hardware/libhardware/* hardware/libhardware
+cd $REPO_DIR
+cp $PATCHES_DIR/patches/hardware/libhardware/* hardware/libhardware
 cd hardware/libhardware
 git apply *.patch
 
 echo Applying system/bpf
-cd $REPODIR
-cp patches/patches/system/bpf/* system/bpf
+cd $REPO_DIR
+cp $PATCHES_DIR/patches/system/bpf/* system/bpf
 cd system/bpf
 git apply *.patch
 
 echo Applying system/netd
-cd $REPODIR
-cp patches/patches/system/netd/* system/netd
+cd $REPO_DIR
+cp $PATCHES_DIR/patches/system/netd/* system/netd
 cd system/netd
 git apply *.patch
 
 echo Applying system/security
-cd $REPODIR
-cp patches/patches/system/security/* system/security
+cd $REPO_DIR
+cp $PATCHES_DIR/patches/system/security/* system/security
 cd system/security
 git apply *.patch
 
