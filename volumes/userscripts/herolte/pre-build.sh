939120b3dd5d7216f7bbf93f69721fbd28a648d7
msx98 <me@glub.xyz>
2023-07-21 16:28:20 +0300

    Part 1: change patching method for herolte

--- volumes/userscripts/herolte/pre-build.sh
+++ volumes/userscripts/herolte/pre-build.sh
@@ -1,30 +1,4 @@
-REPO_DIR=$SRC_DIR/LINEAGE_19_1
-PATCHES_DIR=/srv/patches/device/herolte
-
-cd $REPO_DIR
-bash $PATCHES_DIR/picks.sh
-
-echo Applying hardware/libhardware
-cd $REPO_DIR
-cp $PATCHES_DIR/patches/hardware/libhardware/* hardware/libhardware
-cd hardware/libhardware
-git apply *.patch
-
-echo Applying system/bpf
-cd $REPO_DIR
-cp $PATCHES_DIR/patches/system/bpf/* system/bpf
-cd system/bpf
-git apply *.patch
-
-echo Applying system/netd
-cd $REPO_DIR
-cp $PATCHES_DIR/patches/system/netd/* system/netd
-cd system/netd
-git apply *.patch
-
-echo Applying system/security
-cd $REPO_DIR
-cp $PATCHES_DIR/patches/system/security/* system/security
-cd system/security
-git apply *.patch
-
+cd /srv/src/LINEAGE_19_1/
+cp /root/patches/apply.sh .
+sh apply.sh
+rm apply.sh
